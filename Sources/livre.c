490f1daa35498ec7c7e37cf0c5d1176dd8fcad7b
Salah Eddine Zemmouri <zemm.salaheddine@gmail.com>
2020-12-28 21:59:53 +0100

    update

--- Sources/livre.c
+++ Sources/livre.c
@@ -1,23 +1,25 @@
-#include "livre.h"
+#include <livre.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 Livre scan_livre(){
     Livre lv;
     printf("Numero: ");
-    scanf("%d",&lv.num_liv);
+    scanf("%d%*c",&lv.num_liv); 
+    // le %*c est pour ignorer le caractere espace ('\n') que scanf laisse
+    // pour que fgets ne le lit pas  
     
     printf("\nTitre: ");
-    scanf("%s",lv.titre_liv);
+    fgets(lv.titre_liv,sizeof(lv.titre_liv),stdin);
     
     printf("\nCatalogue: ");
-    scanf("%s",lv.categ_liv);
+    fgets(lv.categ_liv,sizeof(lv.categ_liv),stdin);
     
     printf("\nNom de l'auteur: ");
-    scanf("%s",lv.auteur_liv.nom_aut);
+    fgets(lv.auteur_liv.nom_aut,sizeof(lv.auteur_liv.nom_aut),stdin);
     
     printf("\nPrenom de l'auteur: ");
-    scanf("%s",lv.auteur_liv.prenom_aut);
+    fgets(lv.auteur_liv.prenom_aut,sizeof(lv.auteur_liv.nom_aut),stdin);
     
     printf("\nNumero d'adherant ayant emprunte le livre: ");
     scanf("%d",&lv.emprunteur_liv);
@@ -26,19 +28,25 @@ Livre scan_livre(){
 }
 
 void print_livre(Livre lv){
-    printf("Numero: %d",lv.num_liv);
-    printf("\nTitre: %s",lv.titre_liv);
-    printf("\nCatalogue: %s",lv.categ_liv);
-    printf("\nNom de l'auteur: %s",lv.auteur_liv.nom_aut);
-    printf("\nPrenom de l'auteur: %s",lv.auteur_liv.prenom_aut);
-    printf("\nNumero d'adherant ayant emprunte le livre: %d",lv.emprunteur_liv);
+    printf("Numero: %d\n",lv.num_liv);
+    printf("Titre: %s\n",lv.titre_liv);
+    printf("Catalogue: %s\n",lv.categ_liv);
+    printf("Nom de l'auteur: %s\n",lv.auteur_liv.nom_aut);
+    printf("Prenom de l'auteur: %s\n",lv.auteur_liv.prenom_aut);
+    printf("Numero d'adherant ayant emprunte le livre: %d\n",lv.emprunteur_liv);
 }
 
 void print_all(){
-    FILE *pf=fopen("livre.dat","r+b");
+    FILE *pf=fopen("livre.dat","rb");
+    if(!pf) {
+        printf("cannot open file \n");
+        exit(-1);
+    }
     Livre temp;
     while (fread(&temp,sizeof(Livre),1,pf)==1){
+        printf("--------------\n");
         print_livre(temp);
+        printf("--------------\n");
     }
     fclose(pf);
 }
@@ -47,8 +55,13 @@ void print_all(){
 void ajout_livre(){
     // mode ab pour ajouter a la fin du fichier binaire
     FILE *fin=fopen("livre.dat","ab");
+    if(!fin) {
+        printf("cannot open file \n");
+        exit(-1);
+    }
     //lecture
     Livre lv=scan_livre();
     //ecriture
     fwrite(&lv,sizeof(Livre),1,fin);
+    fclose(fin);
 }
--- Sources/main.c
+++ Sources/main.c
@@ -3,6 +3,10 @@
 #include "livre.h"
 
 int main(){
-
+    //ajout_livre();
+    print_all();
     return 0;
 }
+
+
+// to run : // gcc -I/home/zemmouri/Desktop/BiblioEnsias/Headers livre.c main.c -o main && ./main
